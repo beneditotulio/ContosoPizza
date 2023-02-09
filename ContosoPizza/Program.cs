9e78e20b04d2827e236094763599523d9dbc49a6
Túlio Benedito Nhantumbo <beneditotulio28@gmail.com>
2023-02-10 00:54:35 +0200

    pull Data using Fluent Api and Linq

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ContosoPizza.Data;
+using ContosoPizza.Models;
+
+using ContosoPizzaContext context = new ContosoPizzaContext();
+
+//var products = context.Products
+//    .Where(p => p.Price > 10.00M)
+//    .OrderBy(p => p.Name);
+var products = from product in context.Products
+               where product.Price > 10.00M
+               orderby product.Name
+               select product;
+
+foreach (Product item in products)
+{
+    Console.WriteLine($"Id:     {item.Id}");
+    Console.WriteLine($"Name:     {item.Name}");
+    Console.WriteLine($"Price:     {item.Price}");
+    Console.WriteLine(new string('-', 20));
+}
