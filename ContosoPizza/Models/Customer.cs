6b22e041d2ec4d27f83913863d4cd473d4b35f0a
Túlio Benedito Nhantumbo <beneditotulio28@gmail.com>
2023-02-10 00:51:18 +0200

    Adding Email Property

--- ContosoPizza/Models/Customer.cs
+++ ContosoPizza/Models/Customer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ContosoPizza.Models
+﻿namespace ContosoPizza.Models
 {
     public class Customer
     {
@@ -13,6 +7,7 @@ namespace ContosoPizza.Models
         public string LastName { get; set; } = null!;
         public string? Address { get; set; }
         public string? Phone { get; set; }
+        public string? Email { get; set; }
         public ICollection<Order> Orders { get; set; } = null!;
     }
 }
